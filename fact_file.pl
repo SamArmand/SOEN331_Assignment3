4d754399ecc789e41dfcae9730b774a22d274506
Jessica Falco <j.falco93@gmail.com>
2015-03-27 18:13:44 -0400

    Fact file: reordered some transisitions

--- fact_file.pl
+++ fact_file.pl
@@ -82,15 +82,15 @@ transition(active, exit, kill, '!inlockdown', null).
 %% active transitions
 transition(dormant, init, start, null, 'retry = 0').
 transition(init, idle, init_ok, null, 'retry = 0').
-transition(idle, monitoring, begin_monitoring, null, null).
 transition(init, error_diagnosis, init_crash, null, init_err_msg).
+transition(idle, error_diagnosis, idle_crash, null, idle_err_msg).
+transition(idle, monitoring, begin_monitoring, null, null).
+transition(monitoring, error_diagnosis, monitor_crash, '!inlockdown', moni_err_msg).
 transition(error_diagnosis, init, retry_init, 'retry < 3', 'retry++').
 transition(error_diagnosis, safe_shutdown, shutdown, 'retry >= 3', null).
-transition(safe_shutdown, dormant, sleep, null, null).
-transition(idle, error_diagnosis, idle_crash, null, idle_err_msg).
-transition(error_diagnosis, idle, idle_rescue, null, null).
-transition(monitoring, error_diagnosis, monitor_crash, '!inlockdown', moni_err_msg).
 transition(error_diagnosis, monitoring, moni_rescue, null, null).
+transition(error_diagnosis, idle, idle_rescue, null, null).
+transition(safe_shutdown, dormant, sleep, null, null).
 
 %% init transitions
 transition(boot_hw, senchk, hw_ok, null, null).
