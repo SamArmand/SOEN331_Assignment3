1aa09716063e35fdd270cf71bf384c14e9880d2a
Jessica Falco <j.falco93@gmail.com>
2015-03-27 20:53:07 -0400

    Rules: added all_superstates, all_states, and all_init_states

--- rule_file.pl
+++ rule_file.pl
@@ -9,3 +9,14 @@ size(Length) :- findall([Event, Guard], transition(_, _, Event, Guard, _), L),
 					length(L, Length).
 
 is_link(Event, Guard) :- transition(X, Y, Event, Guard, _), X \= Y.
+
+all_superstates(Set) :- findall(State, superstate(State, _), L),
+						list_to_set(L, Set).
+
+%% ancestor 
+
+%% inherits 
+
+all_states(L) :- findall(State, state(State), L).
+
+all_init_states(L) :- findall(State, initial_state(State, _), L).
