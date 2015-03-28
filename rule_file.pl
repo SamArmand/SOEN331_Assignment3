8337f58123c750a5bbc60c79ea813058922e6b56
Sam Assaf <sam.assaf@live.ca>
2015-03-27 20:43:30 -0400

    added all_loops and is_edge to rule_file

--- rule_file.pl
+++ rule_file.pl
@@ -1,1 +1,6 @@
 is_loop(Event, Guard) :- transition(X, X, Event, Guard, _).
+
+all_loops(Set) :- findall([Event, Guard], transition(X, X, Event, Guard, _), L),
+					list_to_set(L, Set).
+
+is_edge(Event, Guard) :- transition(_, _, Event, Guard, _).
