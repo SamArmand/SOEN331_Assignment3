9d8e167e7889672759f59df9f93fb79fcd510a39
Sam Assaf <sam.assaf@live.ca>
2015-03-27 20:45:32 -0400

    added size and is_link to rule_file

--- rule_file.pl
+++ rule_file.pl
@@ -4,3 +4,8 @@ all_loops(Set) :- findall([Event, Guard], transition(X, X, Event, Guard, _), L),
 					list_to_set(L, Set).
 
 is_edge(Event, Guard) :- transition(_, _, Event, Guard, _).
+
+size(Length) :- findall([Event, Guard], transition(_, _, Event, Guard, _), L),
+					length(L, Length).
+
+is_link(Event, Guard) :- transition(X, Y, Event, Guard, _), X \= Y.
