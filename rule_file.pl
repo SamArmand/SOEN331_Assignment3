3552fd371e69d27487d6a42c86b54405641dd1f7
Sam Assaf <sam.assaf@live.ca>
2015-03-27 21:06:38 -0400

    Added inherits_transitions, state_is_reflexive and graph_is_reflexive

--- rule_file.pl
+++ rule_file.pl
@@ -16,10 +16,19 @@ all_superstates(Set) :- findall(State, superstate(State, _), L),
 ancestor(Ancestor, Descendent) :- superstate(Ancestor, Descendent); 
 									(superstate(Ancestor, X), ancestor(X, Descendent)). 
 
-%% inherits 
+%% Here it was sepcified that we should return a list of transitions. Transtitions are defined as [Source, Destination, Event, Guard, Action]
+inheritss_transitions(State, List) :- findall([Source, Destination, Event, Guard, Action], (ancestor(Source, State), transition(Source, Destination, Event, Guard, Action)), List).
 
 all_states(L) :- findall(State, state(State), L).
 
 all_init_states(L) :- findall(State, initial_state(State, _), L).
 
 get_starting_state(State) :- initial_state(State, null).
+
+state_is_reflexive(State) :- transition(State, State, _, _, _).
+
+graph_is_reflexive :- findall(State, transition(State, State, _, _, _), L1),
+						list_to_set(L1, Set),
+						all_states(L2),
+						same_length(L1, L2).
+
